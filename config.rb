99ec3bb92d97d610aff67ec9a32e39403eae1eac
Jonah Bailey <jonahb@teamddm.com>
2012-10-04 20:34:01 -0400

    taking out custom styles to establish more of a default

--- config.rb
+++ config.rb
@@ -3,7 +3,7 @@ require 'susy'
 
 
 # Set this to the root of your project when deployed:
-http_path = "/includes/files/base/css/scss/"
+http_path = "/"
 css_dir = "stylesheets"
 sass_dir = "sass"
 images_dir = "images"
